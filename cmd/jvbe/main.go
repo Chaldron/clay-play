37850ee74a01a27c1c74ac5a2e037419e3323fdf
Matthew Fan <kungfumo@gmail.com>
2024-01-20 21:36:25 -0600

    set up air and scss

--- cmd/jvbe/main.go
+++ cmd/jvbe/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+	r := chi.NewRouter()
+
+	publicFileServer := http.FileServer(http.Dir("./ui/public"))
+	r.Handle("/public/*", http.StripPrefix("/public/", publicFileServer))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles(
+			"./ui/views/pages/home.html",
+		)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		t.Execute(w, nil)
+	})
+
+	http.ListenAndServe(":8080", r)
 }
